100a2a62e0b02fd47de678b0010af335b375bd11
Jose Gabriel Jiménez <74217610+josegabjimenez@users.noreply.github.com>
2025-01-09 13:04:52 -0500

    Feat/strokee 16/dashboard emergency reception (#4)
    * feat: create dashboard view
    
    * feat: create settings menu, create emergency mockups and connect emergency card with emergency details view
    
    * refactor: remove console.logs
    
    * fix: make login page responsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,27 @@
 'use client';
-import { useAuth } from '@/context/AuthContext';
-import { SignOut } from '@/firebase/config';
+
+// Mocks
+import {emergenciesList} from '@/mocks/emergency';
+
+// Components
+import EmergencyCard from '@/components/EmergencyCard';
+import SettingsMenu from '@/components/SettingsMenu';
 
 export default function Dashboard() {
-  const { user } = useAuth();
+	return (
+		<main className="min-h-screen bg-white p-4">
+			{/* Header */}
+			<SettingsMenu />
 
-  const handleLogOut = async () => {
-    try {
-      await SignOut();
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
+			{/* Main Content */}
+			<div className="mt-12 px-4 flex flex-col items-center w-full">
+				<h1 className="text-3xl font-bold text-gray-900 mb-8">En proceso</h1>
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogOut}>LogOut</button>
-    </div>
-  );
+				{/* Patient Information */}
+				{emergenciesList.map((emergency) => (
+					<EmergencyCard key={emergency.emergencyId} userName={emergency.userName} userPhone={emergency.userPhone} emergencyId={emergency.emergencyId} />
+				))}
+			</div>
+		</main>
+	);
 }
--- app/emergency/[emergencyId]/page.tsx
+++ app/emergency/[emergencyId]/page.tsx
@@ -1,50 +1,40 @@
 'use client';
 
-import React, {useState, useEffect} from 'react'
-import { EmergencyInfo } from '@/types';
+import React, {useState, useEffect} from 'react';
+import {EmergencyInfo} from '@/types';
+import {ArrowBigLeft} from 'lucide-react';
+import Link from 'next/link';
+
+// Mocks
+import {emergency1} from '@/mocks/emergency';
+
+// Components
 import EmergencyInfoComponent from '@/components/EmergencyInfoComponent';
 import ConfirmStrokeComponent from '@/components/ConfirmStrokeComponent';
-import { ArrowBigLeft } from 'lucide-react';
-import Link from 'next/link';
-export default function EmergencyClientPage({
-  params,
-}: {
-  params: Promise<{ emergencyId: string }>;
-}) {
-  const { emergencyId } = React.use(params);
-  const [emergency, setEmergency] = useState< EmergencyInfo | null >(null);
-  const myEmergencyId = emergencyId || "123";
-  //Delete after fetching emergency info
-  const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
-    
-  useEffect(() => {
-    // Fetch emergency info
-    // setEmergency(emergencyInfo);
 
-    // //Delete after fetching emergency info
-    setEmergency({
-      emergencyId: myEmergencyId ?? "123", // Ensure emergencyId is a string
-      userName: "Juan Perez",
-      userPhone: "1234567890",
-      userAge: 30,
-      userAddress: "Calle 123",
-      userWeight: 77.5,
-      userHeight: 170.5,
-      emergencyTime: fiveMinutesAgo,
-      emergencyLocation: "Calle 123",
-    });
+export default function EmergencyClientPage({params}: {params: Promise<{emergencyId: string}>}) {
+	const {emergencyId} = React.use(params);
+	const [emergency, setEmergency] = useState<EmergencyInfo | null>(null);
+	const myEmergencyId = emergencyId || '123';
 
-  }, [emergencyId]);
+	useEffect(() => {
+		// Fetch emergency info
+		// setEmergency(emergencyInfo);
+		// make a new date with the current date and then pass it to a string
 
-  return (
-    <div>
-      <div className='text-customRed mt-4 ml-4'>
-        <Link href='/dashboard'>
-          <ArrowBigLeft size={48}/>
-        </Link>
-      </div>
-      <EmergencyInfoComponent {...emergency}/>
-      <ConfirmStrokeComponent emergencyId={myEmergencyId}/>
-    </div>
-  )
+		// Delete after fetching emergency info
+		setEmergency(emergency1);
+	}, [emergencyId]);
+
+	return (
+		<div>
+			<div className="text-customRed mt-4 ml-4">
+				<Link href="/dashboard">
+					<ArrowBigLeft size={48} />
+				</Link>
+			</div>
+			<EmergencyInfoComponent {...emergency} />
+			<ConfirmStrokeComponent emergencyId={myEmergencyId} />
+		</div>
+	);
 }
