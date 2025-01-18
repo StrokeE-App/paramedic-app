d0bba8a789fa82f9245cf14152721f351bc56d88
Jose Gabriel Jiménez <josegabojimenez@gmail.com>
2025-01-17 22:47:29 -0500

    feat: add toasts to handle errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
 import {AuthProvider} from '@/context/AuthContext';
+import {Toaster} from 'react-hot-toast';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -25,14 +26,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="es">
-      {/* <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        /> */}
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-customWhite`}>
 				<AuthProvider>{children}</AuthProvider>
+				<Toaster position="top-center" />
 			</body>
 		</html>
 	);
